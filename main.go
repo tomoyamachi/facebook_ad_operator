d24dde98678e97dc692ce1a9984a0472432c1936
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-07-26 16:26:13 +0900

    Fix golint error

--- main.go
+++ main.go
@@ -15,31 +15,31 @@ func main() {
 	if err != nil {
 		fmt.Println("Config file not found...")
 	} else {
-		dev_server := viper.GetString("development.server")
-		dev_connection_max := viper.GetInt("development.connection_max")
-		dev_enabled := viper.GetBool("development.enabled")
-		dev_port := viper.GetInt("development.port")
+		devServer := viper.GetString("development.server")
+		devConnectionMax := viper.GetInt("development.connection_max")
+		devEnabled := viper.GetBool("development.enabled")
+		devPort := viper.GetInt("development.port")
 
 		fmt.Printf("\nDevelopment Config found:\n server = %s\n connection_max = %d\n"+
 			" enabled = %t\n"+
 			" port = %d\n",
-			dev_server,
-			dev_connection_max,
-			dev_enabled,
-			dev_port)
+			devServer,
+			devConnectionMax,
+			devEnabled,
+			devPort)
 
-		prod_server := viper.GetString("production.server")
-		prod_connection_max := viper.GetInt("production.connection_max")
-		prod_enabled := viper.GetBool("production.enabled")
-		prod_port := viper.GetInt("production.port")
+		prodServer := viper.GetString("production.server")
+		prodConnectionMax := viper.GetInt("production.connection_max")
+		prodEnabled := viper.GetBool("production.enabled")
+		prodPort := viper.GetInt("production.port")
 
 		fmt.Printf("\nProduction Config found:\n server = %s\n connection_max = %d\n"+
 			" enabled = %t\n"+
 			" port = %d\n",
-			prod_server,
-			prod_connection_max,
-			prod_enabled,
-			prod_port)
+			prodServer,
+			prodConnectionMax,
+			prodEnabled,
+			prodPort)
 	}
 
 }
